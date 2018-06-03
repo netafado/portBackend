a0bc0ab9ccf699a53a7f69dbb1469b2eb4c3cd83
Isaias Francisco dos Santos <isaias.fran@gmail.com>
2018-06-02 23:32:12 -0300

    todos

--- config/cors.js
+++ config/cors.js
@@ -1,14 +1,10 @@
 const express =  require('express');
 
 exports.cors = (req, res, next)=>{
-    var allowedOrigins = ['http://localhost:3000', 'http://isaiasfrancisco.com.br/', 'https://isaiasfrancisco.com.br/'];
-    var origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", "https://isaiasfrancisco.com.br/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true)
     next();    
 }
+
+
